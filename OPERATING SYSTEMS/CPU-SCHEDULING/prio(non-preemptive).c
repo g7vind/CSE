a2359f9781737df4e4c048d2935ba17759cff5d5
g7vind <codingacc408@gmail.com>
2023-08-15 14:45:41 +0530

    updated scheduling programs

--- OPERATING SYSTEMS/CPU-SCHEDULING/prio(non-preemptive).c
+++ OPERATING SYSTEMS/CPU-SCHEDULING/prio(non-preemptive).c
@@ -1,50 +1,76 @@
-#include<stdio.h>
-#include<string.h>
-struct process{
-	char name[20];
-	int at,bt,tt,wt,stat,pr;
-}p[50],temp;
-struct chart{
-	int ct,st;
-	char name[20];
-}g[50];
+#include <stdio.h>
+#include <string.h>
+struct process
+{
+    char name[20];
+    int at, bt, tt, wt, stat, pr;
+    int order;
+} p[50], temp;
+struct chart
+{
+    int ct, st;
+    char name[20];
+} g[50];
 void main()
 {
-	int n,i,j=0,idle=0,flag,l=0,m=0;
-	float avgtt=0,avgwt=0;
-	printf("\nEnter the number of process : ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("\nEnter the process name : ");
-		scanf("%s",p[i].name);
-		printf("\nEnter the arrival time : ");
-		scanf("%d",&p[i].at);
-		printf("\nEnter the burst time : ");
-		scanf("%d",&p[i].bt);
+    int n, i, j = 0, idle = 0, flag, l = 0, m = 0;
+    float avgtt = 0, avgwt = 0;
+    printf("\nEnter the number of process : ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("\nEnter the process name : ");
+        scanf("%s", p[i].name);
+        printf("\nEnter the arrival time : ");
+        scanf("%d", &p[i].at);
+        printf("\nEnter the burst time : ");
+        scanf("%d", &p[i].bt);
         printf("\nEnter the priority number : ");
-        scanf("%d",&p[i].pr);
-		p[i].stat=0;
-	}
-    i=0;
-    while (l < n) {
+        scanf("%d", &p[i].pr);
+        p[i].stat = 0;
+        p[i].order = i;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (p[j].at > p[j + 1].at)
+            {
+                struct process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+    i = 0;
+    while (l < n)
+    {
         int k = -1;
-        for (j = 0; j < n; j++) {
-            if (p[j].stat == 0 && p[j].at <= i && (k == -1 || p[j].pr < p[k].pr)) {
+        for (j = 0; j < n; j++)
+        {
+            if (p[j].stat == 0 && p[j].at <= i && (k == -1 || p[j].pr < p[k].pr))
+            {
                 k = j;
             }
         }
-        if (k == -1) {
-            if (idle == 0) {
+        if (k == -1)
+        {
+            if (idle == 0)
+            {
                 strcpy(g[m].name, "idle");
                 g[m].st = i;
                 idle = 1;
                 i++;
-            } else {
+            }
+            else
+            {
                 i++;
             }
-        } else {
-            if (idle == 1) {
+        }
+        else
+        {
+            if (idle == 1)
+            {
                 g[m].ct = i;
                 m++;
             }
@@ -60,23 +86,37 @@ void main()
             l++;
         }
     }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (p[j].order > p[j + 1].order)
+            {
+                struct process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
     printf("\nProcess Table");
-	printf("\nName \tat \tbt \twt \ttt \tpriority\n");
-	for(i=0;i<n;i++){
-		printf("%s\t %d \t%d\t%d\t%d\t%d\n",p[i].name,p[i].at,p[i].bt,p[i].wt,p[i].tt,p[i].pr);
-		avgwt+=p[i].wt;
-		avgtt+=p[i].tt;}
-	avgwt/=n;
-	avgtt/=n;
+    printf("\nName \tat \tbt \twt \ttt \tpriority\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\t %d \t%d\t%d\t%d\t%d\n", p[i].name, p[i].at, p[i].bt, p[i].wt, p[i].tt, p[i].pr);
+        avgwt += p[i].wt;
+        avgtt += p[i].tt;
+    }
+    avgwt /= n;
+    avgtt /= n;
     printf("\nGantt Chart\n");
-	printf("---------------------------------------------------------\n");
-	printf("|");
-	for(i=0;i<m;i++)
-		printf("%s\t|",g[i].name);
-	printf("\n---------------------------------------------------------\n");
-	printf("%d\t",g[m].st);
-	for(i=0;i<m;i++)
-		printf("%d\t",g[i].ct);
-	printf("\n\nAverage waiting time : %.2f",avgwt);
-	printf("\nAverage turnaround time : %.2f\n",avgtt);
+    printf("---------------------------------------------------------\n");
+    printf("|");
+    for (i = 0; i < m; i++)
+        printf("%s\t|", g[i].name);
+    printf("\n---------------------------------------------------------\n");
+    printf("%d\t", g[m].st);
+    for (i = 0; i < m; i++)
+        printf("%d\t", g[i].ct);
+    printf("\n\nAverage waiting time : %.2f", avgwt);
+    printf("\nAverage turnaround time : %.2f\n", avgtt);
 }
--- OPERATING SYSTEMS/CPU-SCHEDULING/sjf(non-preemptive).c
+++ OPERATING SYSTEMS/CPU-SCHEDULING/sjf(non-preemptive).c
@@ -1,48 +1,74 @@
-#include<stdio.h>
-#include<string.h>
-struct process{
-	char name[20];
-	int at,bt,tt,wt,stat;
-}p[50];
-struct chart{
-	int ct,st;
-	char name[20];
-}g[50];
+#include <stdio.h>
+#include <string.h>
+struct process
+{
+    char name[20];
+    int at, bt, tt, wt, stat;
+    int order;
+} p[50];
+struct chart
+{
+    int ct, st;
+    char name[20];
+} g[50];
 void main()
 {
-	int n,i,j=0,idle=0,flag,l=0,m=0;
-	float avgtt=0,avgwt=0;
-	printf("\nEnter the number of process : ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("\nEnter the process name : ");
-		scanf("%s",p[i].name);
-		printf("\nEnter the arrival time : ");
-		scanf("%d",&p[i].at);
-		printf("\nEnter the burst time : ");
-		scanf("%d",&p[i].bt);
-		p[i].stat=0;
-	}
-    i=0;
-    while (l < n) {
+    int n, i, j = 0, idle = 0, flag, l = 0, m = 0;
+    float avgtt = 0, avgwt = 0;
+    printf("\nEnter the number of process : ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("\nEnter the process name : ");
+        scanf("%s", p[i].name);
+        printf("\nEnter the arrival time : ");
+        scanf("%d", &p[i].at);
+        printf("\nEnter the burst time : ");
+        scanf("%d", &p[i].bt);
+        p[i].stat = 0;
+        p[i].order = i;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (p[j].at > p[j + 1].at)
+            {
+                struct process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+    i = 0;
+    while (l < n)
+    {
         int k = -1;
-        for (j = 0; j < n; j++) {
-            if (p[j].stat == 0 && p[j].at <= i && (k == -1 || p[j].bt < p[k].bt)) {
+        for (j = 0; j < n; j++)
+        {
+            if (p[j].stat == 0 && p[j].at <= i && (k == -1 || p[j].bt < p[k].bt))
+            {
                 k = j;
             }
         }
-        if (k == -1) {
-            if (idle == 0) {
+        if (k == -1)
+        {
+            if (idle == 0)
+            {
                 strcpy(g[m].name, "idle");
                 g[m].st = i;
                 idle = 1;
                 i++;
-            } else {
+            }
+            else
+            {
                 i++;
             }
-        } else {
-            if (idle == 1) {
+        }
+        else
+        {
+            if (idle == 1)
+            {
                 g[m].ct = i;
                 m++;
             }
@@ -58,23 +84,37 @@ void main()
             l++;
         }
     }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (p[j].order > p[j + 1].order)
+            {
+                struct process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
     printf("\nProcess Table");
-	printf("\nName \tat \tbt \twt \ttt\n");
-	for(i=0;i<n;i++){
-		printf("%s\t %d \t%d\t%d\t%d\n",p[i].name,p[i].at,p[i].bt,p[i].wt,p[i].tt);
-		avgwt+=p[i].wt;
-		avgtt+=p[i].tt;}
-	avgwt/=n;
-	avgtt/=n;
+    printf("\nName \tat \tbt \twt \ttt\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\t %d \t%d\t%d\t%d\n", p[i].name, p[i].at, p[i].bt, p[i].wt, p[i].tt);
+        avgwt += p[i].wt;
+        avgtt += p[i].tt;
+    }
+    avgwt /= n;
+    avgtt /= n;
     printf("\nGantt Chart\n");
-	printf("---------------------------------------------------------\n");
-	printf("|");
-	for(i=0;i<m;i++)
-		printf("%s\t|",g[i].name);
-	printf("\n---------------------------------------------------------\n");
-	printf("%d\t",g[m].st);
-	for(i=0;i<m;i++)
-		printf("%d\t",g[i].ct);
-	printf("\n\nAverage waiting time : %.2f",avgwt);
-	printf("\nAverage turnaround time : %.2f\n",avgtt);
+    printf("---------------------------------------------------------\n");
+    printf("|");
+    for (i = 0; i < m; i++)
+        printf("%s\t|", g[i].name);
+    printf("\n---------------------------------------------------------\n");
+    printf("%d\t", g[m].st);
+    for (i = 0; i < m; i++)
+        printf("%d\t", g[i].ct);
+    printf("\n\nAverage waiting time : %.2f", avgwt);
+    printf("\nAverage turnaround time : %.2f\n", avgtt);
 }
