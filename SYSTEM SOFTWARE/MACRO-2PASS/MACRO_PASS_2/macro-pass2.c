e7e4db2118e3b691b2604052aebd6c42287aafc9
g7vind <govindmh003@gmail.com>
2023-12-09 19:16:24 +0530

    updated

--- SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_2/macro-pass2.c
+++ SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_2/macro-pass2.c
@@ -3,62 +3,62 @@
 #include <stdlib.h>
 void main()
 {
-    FILE *f1, *f2, *f3, *f4, *f5;
+    FILE *finput, *fnamTab, *fdefTab, *fargTab, *fexpTab;
     int i, len;
     char mne[20], opnd[20], la[20], name[20], mne1[20], opnd1[20], arg[20];
-    f1 = fopen("minp2.txt", "r");
-    f2 = fopen("ntab2.txt", "r");
-    f3 = fopen("dtab2.txt", "r");
-    f4 = fopen("atab2.txt", "w+");
-    f5 = fopen("op2.txt", "w");
-    fscanf(f1, "%s%s%s", la, mne, opnd);
+    finput = fopen("minp2.txt", "r");
+    fnamTab = fopen("ntab2.txt", "r");
+    fdefTab = fopen("dtab2.txt", "r");
+    fargTab = fopen("atab2.txt", "w+");
+    fexpTab = fopen("op2.txt", "w");
+    fscanf(finput, "%s%s%s", la, mne, opnd);
     while (strcmp(mne, "END") != 0)
     {
         if (strcmp(mne, "MACRO") == 0)
         {
-            fscanf(f1, "%s%s%s", la, mne, opnd);
+            fscanf(finput, "%s%s%s", la, mne, opnd);
             while (strcmp(mne, "MEND") != 0)
-                fscanf(f1, "%s%s%s", la, mne, opnd);
+                fscanf(finput, "%s%s%s", la, mne, opnd);
         }
         else
         {
-            fscanf(f2, "%s", name);
+            fscanf(fnamTab, "%s", name);
             if (strcmp(mne, name) == 0)
             {
                 len = strlen(opnd);
                 for (i = 0; i < len; i++)
                 {
                     if (opnd[i] != ',')
-                        fprintf(f4, "%c", opnd[i]);
+                        fprintf(fargTab, "%c", opnd[i]);
                     else
-                        fprintf(f4, "\n");
+                        fprintf(fargTab, "\n");
                 }
-                rewind(f2);
-                rewind(f4);
-                fscanf(f3, "%s%s", mne1, opnd1);
-                fscanf(f3, "%s%s", mne1, opnd1);
+                rewind(fnamTab);
+                rewind(fargTab);
+                fscanf(fdefTab, "%s%s", mne1, opnd1);
+                fscanf(fdefTab, "%s%s", mne1, opnd1);
                 while (strcmp(mne1, "MEND") != 0)
                 {
                     if ((opnd1[0] == '&'))
                     {
-                        fscanf(f4, "%s", arg);
-                        fprintf(f5, "-\t%s\t%s\n", mne1, arg);
+                        fscanf(fargTab, "%s", arg);
+                        fprintf(fexpTab, "-\t%s\t%s\n", mne1, arg);
                     }
                     else
-                        fprintf(f5, "-\t%s\t%s\n", mne1, opnd1);
-                    fscanf(f3, "%s%s", mne1, opnd1);
+                        fprintf(fexpTab, "-\t%s\t%s\n", mne1, opnd1);
+                    fscanf(fdefTab, "%s%s", mne1, opnd1);
                 }
             }
             else
-                fprintf(f5, "%s\t%s\t%s\n", la, mne, opnd);
+                fprintf(fexpTab, "%s\t%s\t%s\n", la, mne, opnd);
         }
-        fscanf(f1, "%s%s%s", la, mne, opnd);
+        fscanf(finput, "%s%s%s", la, mne, opnd);
     }
-    fprintf(f5, "%s\t%s\t%s\n", la, mne, opnd);
-    fclose(f1);
-    fclose(f2);
-    fclose(f3);
-    fclose(f4);
-    fclose(f5);
+    fprintf(fexpTab, "%s\t%s\t%s\n", la, mne, opnd);
+    fclose(finput);
+    fclose(fnamTab);
+    fclose(fdefTab);
+    fclose(fargTab);
+    fclose(fexpTab);
     printf("Pass 2 is successful");
 }
