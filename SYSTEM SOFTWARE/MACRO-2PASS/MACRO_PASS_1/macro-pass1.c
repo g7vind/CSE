b30f9d4def942ebcd8fe7ce4ecd807cbb63356c5
g7vind <govindmh003@gmail.com>
2023-12-09 23:33:42 +0530

    updated macro 2 pass

--- SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_1/macro-pass1.c
+++ SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_1/macro-pass1.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void main()
-{
-    FILE *f1, *f2, *f3;
+
+int main() {
+    FILE *finput, *fnamTab, *fdefTab;
     char mne[20], opnd[20], la[20];
-    f1 = fopen("minp2.txt", "r");
-    f2 = fopen("ntab2.txt", "w+");
-    f3 = fopen("dtab2.txt", "w+");
-    fscanf(f1, "%s%s%s", la, mne, opnd);
-    while (strcmp(mne, "MEND") != 0)
-    {
-        if (strcmp(mne, "MACRO") == 0)
-        {
-            fprintf(f2, "%s\n", la);
-            fprintf(f3, "%s\t%s\n", la, opnd);
-        }
-        else
-            fprintf(f3, "%s\t%s\n", mne, opnd);
-        fscanf(f1, "%s%s%s", la, mne, opnd);
+    finput = fopen("minp2.txt", "r");
+    fnamTab = fopen("ntab2.txt", "w+");
+    fdefTab = fopen("dtab2.txt", "w+");
+    if(finput == NULL || fnamTab == NULL || fdefTab == NULL) {
+        printf("Error opening file\n");
+        exit(0);
     }
-    fprintf(f3, "%s", mne);
-    fclose(f1);
-    fclose(f2);
-    fclose(f3);
-    printf("PASS 1 is successful");
+    while (fscanf(finput, "%s%s%s", la, mne, opnd) != EOF) {
+        if (strcmp(mne, "MACRO") == 0) {
+            fprintf(fnamTab, "%s\n", la);
+            fprintf(fdefTab, "%s\t%s\n", la, opnd);
+            fscanf(finput, "%s%s%s", la, mne, opnd); 
+            while (strcmp(mne, "MEND") != 0) {
+                fprintf(fdefTab, "%s\t%s\n", mne, opnd);
+                fscanf(finput, "%s%s%s", la, mne, opnd);
+            }
+            fprintf(fdefTab, "%s **\n", mne); 
+        }
+    }
+    fclose(finput);
+    fclose(fnamTab);
+    fclose(fdefTab);  
+    printf("PASS 1 is successful\n");
+    return 0;
 }
