d754792d53b905f6780d4dc6987dff14b295b3d8
g7vind <govindmh003@gmail.com>
2023-12-09 19:26:30 +0530

    updated

--- SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_2/macro-pass2.c
+++ SYSTEM SOFTWARE/MACRO-2PASS/MACRO_PASS_2/macro-pass2.c
@@ -14,6 +14,8 @@ void main()
     fscanf(finput, "%s%s%s", la, mne, opnd);
     while (strcmp(mne, "END") != 0)
     {
+        /* This code block is checking if the value of the variable `mne` is equal to the string
+        "MACRO". If it is, it means that a macro definition is encountered in the input file. */
         if (strcmp(mne, "MACRO") == 0)
         {
             fscanf(finput, "%s%s%s", la, mne, opnd);
@@ -25,9 +27,16 @@ void main()
             fscanf(fnamTab, "%s", name);
             if (strcmp(mne, name) == 0)
             {
+                /* The code block is calculating the length of the string `opnd` using the `strlen`
+                function and storing it in the variable `len`. */
                 len = strlen(opnd);
                 for (i = 0; i < len; i++)
                 {
+                    /* The code block is checking each character of the `opnd` string. If the character
+                    is not a comma (','), it is written to the file `fargTab` using the `fprintf`
+                    function. If the character is a comma, a newline character ('\n') is written to
+                    the file `fargTab` using `fprintf`. This code is essentially separating the
+                    operands in `opnd` by writing them on separate lines in the `fargTab` file. */
                     if (opnd[i] != ',')
                         fprintf(fargTab, "%c", opnd[i]);
                     else
@@ -37,13 +46,20 @@ void main()
                 rewind(fargTab);
                 fscanf(fdefTab, "%s%s", mne1, opnd1);
                 fscanf(fdefTab, "%s%s", mne1, opnd1);
+                /* The code block is responsible for expanding the macro definition. It reads the lines
+                from the definition table (`fdefTab`) until it encounters the "MEND" directive,
+                indicating the end of the macro definition. */
                 while (strcmp(mne1, "MEND") != 0)
                 {
+                    /* This code block is checking if the first character of the `opnd1` string is '&'.
+                    If it is, it means that the operand is a macro parameter. */
                     if ((opnd1[0] == '&'))
                     {
                         fscanf(fargTab, "%s", arg);
                         fprintf(fexpTab, "-\t%s\t%s\n", mne1, arg);
                     }
+                   /* The line `else fprintf(fexpTab, "-\t%s\t%s\n", mne1, opnd1);` is responsible for
+                   writing the expanded macro definition to the output file `fexpTab`. */
                     else
                         fprintf(fexpTab, "-\t%s\t%s\n", mne1, opnd1);
                     fscanf(fdefTab, "%s%s", mne1, opnd1);
